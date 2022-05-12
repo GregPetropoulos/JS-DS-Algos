8756c09adc079c88a86c3f32b9141e9931aa1086
GP <gregpetropoulos@yahoo.com>
2022-05-12 05:51:50 -0400

    Worked through the Capitalize problem, capitalize every first letter of a string of words

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,39 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// * Solution #1 Best Answer
+function capitalize(str) {
+  // Make an empty array to store result
+  let words = [];
+
+  // Loop through an array that splits each word in the string by space
+  for (let word of str.split(' ')) {
+    // During iteration upper case the first letter of each word (item)
+    // Slice after the first letter and make it lower case
+    // Concatenate the word back together
+    // Push into empty words array
+    words.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+    // The result here would like [Hello, World, ]
+  }
+
+  //Join the words together with a space character
+  return words.join(' ');
+}
+
+// * Solution #2
+function capitalize(str) {
+  // Capitalize the the first letter of the string to handel limitation of a string passed with an already capitalize first letter
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    // look to left of current character and if there is a space then uppercase and add to result string
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
